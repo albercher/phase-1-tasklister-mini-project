fab489d4132a720090baf5f0d54477c3b0149f84
Abbigale Bercher <abbeybercher@gmail.com>
2021-11-04 10:51:57 -0400

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    document.querySelector("form").addEventListener("submit", (e) => {
+      e.preventDefault()
+      buildTask(e.target["new-task-description"].value) 
+        //use brackets since id has hyphens, but you could do .new_task_description if it wasnt hyphenated
+      
+    })
 });
+
+function buildTask(task){
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = "X";
+  p.textContent = `${task} `;
+  p.appendChild(btn);
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e){
+    e.target.parentNode.remove();
+}
+
+function handleAsc(e){
+  e.target.tasks.sort();
+}
+
+// let myToDos = document.querySelector("#list h2");
+// let sortAsc = document.createElement('button');
+// document.querySelector(`#list`).insertBefore(sortAsc, myToDos)
+// // document.querySelector('#list').appendChild(sortAsc);
+// sortAsc.addEventListener('click', handleAsc);
+// sortAsc.textContent = "Asc";
